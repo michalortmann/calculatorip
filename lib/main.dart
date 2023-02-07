22f5564e05c880a121183feb8c04488d3230d5c5
Michal <smilof@wp.pl>
2023-02-07 10:08:33 +0100

    beta

--- lib/main.dart
+++ lib/main.dart
@@ -11,105 +11,325 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget calcbutton(String btntxt, Color btncolor, Color txtcolor) {
+    return Container(
+
+      child: ElevatedButton(onPressed: (){
+        calculation(btntxt);
+      },
+
+        child: Text(btntxt,
+          style: TextStyle(
+              fontSize: 25,
+              color: txtcolor,
+              fontWeight: FontWeight.bold
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: btncolor,
+          shape: CircleBorder(),
+        ),
+
+      ),
+    );
   }
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [ Padding(padding: EdgeInsets.all(20),
+                child: Text(text,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(color: Colors.white,
+                      fontSize: 100),
+                ),
+              )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(padding: EdgeInsets.only(bottom: 100),),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('AC', Colors.white, Colors.black),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('', Colors.black, Colors.black),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('%', Colors.white, Colors.black),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('/', Colors.orange, Colors.white),
+                ),
+
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(padding: EdgeInsets.only(bottom: 100),),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('7', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('8', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('9', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('x', Colors.orange, Colors.white),
+                ),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(padding: EdgeInsets.only(bottom: 100),),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('4', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('5', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('6', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('-', Colors.orange, Colors.white),
+                ),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(padding: EdgeInsets.only(bottom: 100),),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('1', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('2', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('3', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('+', Colors.orange, Colors.white),
+                ),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(padding: EdgeInsets.only(bottom: 100),),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('+/-', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('0', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton(',', Colors.grey, Colors.white),
+                ),
+                SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: calcbutton('=', Colors.orange, Colors.white),
+                ),
+
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  dynamic text ='0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  void calculation(btnText) {
+
+
+    if(btnText  == 'AC') {
+      text ='0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+
+    } else if( opr == '=' && btnText == '=') {
+
+      if(preOpr == '+') {
+        finalResult = add();
+      } else if( preOpr == '-') {
+        finalResult = sub();
+      } else if( preOpr == 'x') {
+        finalResult = mul();
+      } else if( preOpr == '/') {
+        finalResult = div();
+      }
+
+    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
+
+      if(numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
+      }
+
+      if(opr == '+') {
+        finalResult = add();
+      } else if( opr == '-') {
+        finalResult = sub();
+      } else if( opr == 'x') {
+        finalResult = mul();
+      } else if( opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    }
+    else if(btnText == '%') {
+      result = numOne / 100;
+      finalResult = doesContainDecimal(result);
+    } else if(btnText == '.') {
+      if(!result.toString().contains('.')) {
+        result = result.toString()+'.';
+      }
+      finalResult = result;
+    }
+
+    else if(btnText == '+/-') {
+      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-'+result.toString();
+      finalResult = result;
+
+    }
+
+    else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+
+    setState(() {
+      text = finalResult;
+    });
+
+  }
+
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+
+  String doesContainDecimal(dynamic result) {
+
+    if(result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if(!(int.parse(splitDecimal[1]) > 0))
+        return result = splitDecimal[0].toString();
+    }
+    return result;
+  }
+
 }
